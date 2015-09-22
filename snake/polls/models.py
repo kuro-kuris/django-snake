dd929bdb7e45f878b0ad416a64306ab3b5e8dce6
kuro-kuris <cswart@outlook.com>
2015-09-22 17:14:41 +0100

    admin template customised

--- snake/polls/models.py
+++ snake/polls/models.py
@@ -1,4 +1,4 @@
-import
+import datetime
 
 from django.db import models
 from django.utils import timezone
@@ -8,7 +8,10 @@ class Question(models.Model):
 	pub_date = models.DateTimeField('date published')
 
 	def __unicode__(self):
-		return string(self.question_text)
+		return str(self.question_text)
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question)
@@ -16,7 +19,5 @@ class Choice(models.Model):
 	votes = models.IntegerField(default=0)
 
 	def __unicode__(self):
-		return string(self.choice_text)
+		return str(self.choice_text)
 
-	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
