ea0b8ba29a38163fa067a592d1a9d9a822379191
kuro-kuris <cswart@outlook.com>
2015-09-22 15:49:25 +0100

    string representation corrected for python 2.7

--- snake/polls/models.py
+++ snake/polls/models.py
@@ -1,16 +1,22 @@
+import
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 
-	def __str__(self):
-		return self.question_text
+	def __unicode__(self):
+		return string(self.question_text)
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
 
-	def __str__(self):
-		return self.choice_text
+	def __unicode__(self):
+		return string(self.choice_text)
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
