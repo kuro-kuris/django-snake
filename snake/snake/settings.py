530ae567e3370fe623f11825bab636c53e9b1438
kuro-kuris <cswart@outlook.com>
2015-10-10 20:36:29 +0100

    golden egg admin

--- snake/snake/settings.py
+++ snake/snake/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',
+#    'polls',
+    'egg',
 )
 
 MIDDLEWARE_CLASSES = (
