3544dbc90eaaa04a71d754e32349d959f593999c
kuro-kuris <cswart@outlook.com>
2015-10-10 20:35:51 +0100

    gamified concepts for pets

--- snake/egg/models.py
+++ snake/egg/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
 
-# encapsulating data
-class Saving_Account(models.Model):
-    savings_id = models.IntegerField(default=0)
-    balance = models.IntegerField(default=0)
 
-class Milestone
+
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    budget = models.IntegerField(default=0)
+
+    def __str__(self):
+    	return self.username
+
+class Saving_Account(models.Model):
+    balance = models.IntegerField(default=0)
+    username = models.ForeignKey(User)
+
+    def __str__(self):
+    	return (self.username + " savings account with " + self.balance)
+
+class Pet(models.Model):
+    username = models.ForeignKey(User)
+    name = models.CharField(max_length=200)
+    pet_health = models.IntegerField(default=100)
+    virtual_gold = models.IntegerField(default=0)
+    level = models.IntegerField(default=0)
+    experience = models.IntegerField(default=0)
