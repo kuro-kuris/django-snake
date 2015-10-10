4b3a66e3c01bb568f4cba6f078023799eb0e3b9f
Bence Karpati <s1345679@sms.ed.ac.uk>
2015-10-10 17:16:14 +0100

    Added JSON parsing

--- obp/anyad.py
+++ obp/anyad.py
@@ -35,14 +35,39 @@ redirect_response = input('Paste the full redirect URL here:')
 openbank.parse_authorization_response(redirect_response)
 openbank.fetch_access_token(access_token_url)
 
+#get the bank accounts of the user
 our_bank = 'rbs-rbs-c'
-
-
+print("Available accounts")
 r = openbank.get(u"{}/obp/v1.4.0/banks/{}/accounts/private".format(base_url, our_bank))
+
 accounts = r.json()['accounts']
+for a in accounts:
+    print(a['id'])
+    print(a["label"])
+our_account = accounts[0]['id']
+
+
+r = openbank.get(u"{}/obp/v1.4.0/banks/{}/accounts/{}/owner/transactions".format(base_url, 
+    our_bank,
+    our_account), headers= {'obp_limit' : '25'})
 transactions = r.json()['transactions']
 
+transactions = r.json()
+#parsed_json = json.loads(r.json())
+trans_infos = []
+for trans in transactions["transactions"]:
+    parsed_data = {}
+    parsed_data["this_acc_id"] = trans["this_account"]["id"]
+    for i in range(len(trans["this_account"]["holders"])):
+        parsed_data["this_acc_holder_name_" + str(i)] = trans["this_account"]["holders"][i]["name"]
+    parsed_data["other_acc_id"] = trans["other_account"]["id"]
+    parsed_data["other_acc_holder_name"] = trans["other_account"]["holder"]["name"]
+    parsed_data["transaction_type"] = trans["details"]["type"]
+    parsed_data["transaction_posted"] = trans["details"]["posted"]
+    parsed_data["transaction_completed"] = trans["details"]["completed"]
+    parsed_data["transaction_value"] = trans["details"]["value"]["amount"]
+    trans_infos.append(parsed_data)
 
-print(r)
-print(accounts)
-print(transactions)
+#print(trans_infos)
+for elem in trans_infos:
+    print(elem["transaction_value"])
